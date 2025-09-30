d272cd440ffbfee28172f24c21fbc82ff1f1ad50
theresepeter <161300556+theresepeter@users.noreply.github.com>
2025-09-30 11:32:20 +0200

    add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function hideMenu() {
+    var x = document.getElementById("menu-burger");
+    x.className += "hide";
+    var y = document.getElementById("burger");
+    y.className = "";
+}
+
+function showMenu() {
+    var x = document.getElementById("burger");
+    x.className += "hide";
+    var y = document.getElementById("menu-burger");
+    y.className = "";
+}
+
